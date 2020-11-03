463c70da886cbc75dcb23ea30a1bb54e67eea4c7
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-11-02 20:17:56 -0500

    Color to background.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('I Am Poor'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Image(
-          image: AssetImage('images/i_am_poor_image.png'),
-          fit: BoxFit.cover,
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff386ce5),
+        appBar: AppBar(
+          title: Text('I Am Poor'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/i_am_poor_image.png'),
+            fit: BoxFit.scaleDown,
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
